eeda819998b426e6e7b0bb3ec5634b63c8ee0ce5
Muthomi Mate <muthomimate@gmail.com>
2020-10-10 19:54:51 +0300

    fix styled components error

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,12 +1,13 @@
 import React from "react";
-import { shallow } from "enzyme";
+import { shallow, configure } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 import App from "../App";
+
+configure({ adapter: new Adapter() });
 
 describe("<App />", () => {
   test("should render without crashing", () => {
     const wrapper = shallow(<App />);
     expect(wrapper).toMatchSnapshot();
-    // const { baseElement } = documentBody;
-    // expect(baseElement).toMatchSnapshot();
   });
 });
